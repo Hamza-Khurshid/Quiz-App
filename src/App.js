ad08a0cbc1677c1db72e8d7c17b387661d662ba4
Hamza-Khurshid <hamzakhurshid5509@gmail.com>
2019-02-11 20:44:24 -0800

    Setting Up Basics

--- src/App.js
+++ src/App.js
@@ -1,28 +1,87 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { hot } from 'react-hot-loader';
 import './App.css';
+import Signin from './components/login/Login';
+
 
 class App extends Component {
+  constructor(props) { 
+    super(props);
+
+    this.state = {
+      users: [
+        {
+          id: 1,
+          name: 'Ali',
+          pass: 'ali123'
+        },
+        {
+          id: 2,
+          name: 'Zain',
+          pass: 'zain123'
+        }
+      ], 
+      courses: [
+        {
+          name: 'JavaScript',
+          test: [ {
+            id: 1,
+            name: 'JavaScript Quiz 1',
+            key: 'javascript1',
+            content: [ {
+              qNo: 1,
+              qStmnt: '',
+              qChoice: ['', '', '', ''],
+              ans: ['']
+            } ]
+          }, {
+            id: 2,
+            name: 'JavaScript Quiz 2',
+            key: 'javascript2',
+            content: [ {
+              qNo: 1,
+              qStmnt: '',
+              qChoice: ['', '', '', ''],
+              ans: ['']
+            } ]
+          } ] 
+        }, 
+        {
+          name: 'Python',
+          test: [ {
+            id: 1,
+            name: 'Python Quiz 1',
+            key: 'python1',
+            content: [ {
+              qNo: 1,
+              qStmnt: '',
+              qChoice: ['', '', '', ''],
+              ans: ['']
+            } ]
+          }, {
+            id: 2,
+            name: 'Python Quiz 2',
+            key: 'python2',
+            content: [ {
+              qNo: 1,
+              qStmnt: '',
+              qChoice: ['', '', '', ''],
+              ans: ['']
+            } ]
+          } ] 
+        }
+      ]
+    }
+
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Signin />
       </div>
     );
   }
 }
 
-export default App;
+export default hot(module)(App);
